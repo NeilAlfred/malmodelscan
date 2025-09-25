5a7940a8af77fbba20bb377a7bf049d605ad86a2
NeilAlfred <neilalfred93@gmail.com>
2025-09-25 23:27:07 +0800

    update package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,9 @@
 python -m venv .venv
+# Linux / macOS
 source .venv/bin/activate
+
+# Windows
+# .venv\Scripts\activate
 pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller --onefile main.py
